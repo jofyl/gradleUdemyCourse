799d007ec506d0b8d273e85a2ef8deeebf9dec74
Joseph Loutfi <joseph@anghami.com>
2022-10-05 16:40:11 +0400

    chore: groovy intro, data types, var declaration and regex

---
+++ groovyDemo/src/com/hello/Main.groovy
@@ -0,0 +1,43 @@
+package com.hello
+
+static void main(String[] args) {
+//  println "Hello world!"
+//  float a = 12.4F
+//  println(a)
+//  println(a.class)
+//
+//  String s = "Hello";
+//  println("this is ${s}")
+//
+//  x = "string without typ"
+}
+
+println "Hello world!"
+float a = 12.4F
+println(a)
+println(a.class)
+
+otherA = 12.4
+println(otherA.class)
+
+String s = "Hello";
+println("this is ${s}")
+
+x = "string without type"
+println("this is ${x}")
+
+int z = 1
+d = 2
+println(1 + 2)
+
+p='''
+hello mister x
+what is your name?
+'''
+println(p)
+
+emailPatern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+print "test@gmail.com" ==~ emailPatern
+
+def u = "james"
+def i = 1
